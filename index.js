f063009b7e429b201406d3551dd570e064383314
Meheknaz1704 <meheknaz258@gmail.com>
2021-07-24 11:55:35 +0530

    javascript

---
+++ index.js
@@ -0,0 +1,38 @@
+//Dynamically typed
+//Statically typed
+//javascript is an dynamically typed
+
+//datatypes
+//1. Primitive datatype
+//2. Refferential datatype
+
+//Primitive datatype
+//1.number
+//2.string
+//3.boolean
+//4.null
+//4.undefined
+//5.symbol
+
+var a=20;
+var b="kadur";
+var c=false;
+var d=null
+var s;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(s);
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof s);
+
+// (Hosting)
+console.log(a);
+var a;
+
